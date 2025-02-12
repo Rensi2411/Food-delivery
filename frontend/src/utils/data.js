333280a172d2df44e5c082e59d68901466c15119
Rensi <rensidudhat2411@gmail.com>
2025-02-12 19:25:57 +0530

    Initial commit

---
+++ frontend/src/utils/data.js
@@ -0,0 +1,57 @@
+export const category = [
+  {
+    img: "https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/1:1/w_2560%2Cc_limit/Smashburger-recipe-120219.jpg",
+    name: "Burgers",
+    off: "20-40% OFF",
+  },
+  {
+    img: "https://media.istockphoto.com/id/839988946/photo/whats-on-your-pizza.jpg?s=612x612&w=0&k=20&c=5PkXCPJivtGk68sbbTZvypqMOGpxkF-NdbRZzTSFqds=",
+    name: "Pizzas",
+    off: "10-20% OFF",
+  },
+  {
+    img: "https://www.madhuseverydayindian.com/wp-content/uploads/2022/11/easy-vegetable-biryani.jpg",
+    name: "Biriyanis",
+    off: "20-40% OFF",
+  },
+  {
+    img: "https://static.vecteezy.com/system/resources/thumbnails/035/985/777/small/ai-generated-tcookie-plate-dessert-food-photo.jpg",
+    name: "Desserts",
+    off: "30-40% OFF",
+  },
+  {
+    img: "https://vinut.com.vn/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2023/05/non-alcoholic-drinks-10-recipes-for-a-refreshing-beverage-experience-6452241f36047.jpg.webp",
+    name: "Drinks",
+    off: "10-40% OFF",
+  },
+
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOtHkeg3ozBJdDw_AxSa-F_y7X1UDMc8VjKw&s",
+    name: "Noodles",
+    off: "30-40% OFF",
+  },
+];
+
+export const filter = [
+  {
+    name: "Food Categories",
+    value: "category",
+    items: [
+      "Burger",
+      "Pizza",
+      "Sandwiches",
+      "Biryani",
+      "Pasta",
+      "Salads",
+      "Tacos",
+      "Noodles",
+      "Drinks",
+      "Dessert",
+    ],
+  },
+  {
+    name: "Filter by Price",
+    value: "price",
+    items: [],
+  },
+];
